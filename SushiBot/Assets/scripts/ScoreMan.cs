7b2b6b9a27536c1093233967aed169a41bb3c526
Cody Leeman <codyleeman383@hotmail.com>
2016-09-17 00:40:19 -0400

    SushiBot

---
+++ SushiBot/Assets/scripts/ScoreMan.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreMan : MonoBehaviour {
+
+    public static int score;
+    public Text scoreText;
+
+    public Text hiScoreText;
+    public float hiScoreCount;
+
+
+
+	void Start ()
+    {
+       /* scoreText = GetComponent<Text>();
+        hiScoreText = GetComponent<Text>();
+        score = 0;
+        */
+	}
+	
+	
+	void Update ()
+    {
+        if (score < 0)
+            score = 0;
+
+        if(score > hiScoreCount)
+        {
+            hiScoreCount = score;
+        }
+
+        scoreText.text = "Score: " + score;
+        hiScoreText.text = "High Score: " + hiScoreCount;
+           
+	}
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+
+}
---
+++ SushiBot/Assets/scripts/SushiPick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SushiPick : MonoBehaviour {
+
+    public int points;
+
+    void OnTriggerEnter2d(Collider2D other)
+    {
+       /* if (other.GetComponent<Platformer2DUserController>() == null)
+            return;
+
+        ScoreMan.AddPoints(points);
+        Destroy(gameObject);
+        */
+    }
+
+}
